e3a00eecebd74388f7f9a4dc043017856ebc9c85
alsJ7010 <38120584+alsJ7010@users.noreply.github.com>
2018-04-11 11:12:53 +0900

    Create Cal.c

---
+++ Cal.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+void add(num1, num1);
+
+voidi main()
+{
+  double num1, num2;
+  double result = num1 + num2;
+  
+  printf("num1 : ");
+  scanf("%lf",&num1);
+  
+  printf("num2 : ");
+  scanf("%lf",&num2);
+  
+}
+void add(num1, num1)
+{
+  printf("%lf + %lf = %lf\n", num1, num2, result);
+  return ;
+}
